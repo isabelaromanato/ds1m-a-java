31af02c49b0cd515c715af41953e4afe1740c5e8
ISABELA VICTORIA DE NOVAIS ROMANATO <21193062@CFP127ALUNO.LOCAL>
2021-03-03 11:20:54 -0300

    Aula 03-03-2021

---
+++ conversor_temperatura/src/conversor_temperatura/CelsiusParaFahreheit.java
@@ -0,0 +1,16 @@
+package conversor_temperatura;
+
+public class CelsiusParaFahreheit {
+
+	public static void main(String[] args) {
+		
+		float celsius= 24.0F;
+		float fahreheit;
+		
+		fahreheit = (9* celsius +160)/ 5;
+		
+		System.out.println (fahreheit + " graus pahreheit");
+		
+		
+	}
+}
